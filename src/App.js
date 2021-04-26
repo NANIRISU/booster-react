c388d7980bb2547815ae5a08c2ef3c42dc47a6a9
NANIRISU <39872073+NANIRISU@users.noreply.github.com>
2021-04-26 21:35:44 +0530

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const api = `https://randomuser.me/api`;
+
+  const [user, setUser] = useState("Nothing Inside");
+
+  const addUserHandler = async () => {
+    const userData = await fetch(api, {
+      method: "GET"
+    });
+    const userJson = await userData.json();
+    console.log(userJson);
+    setUser(userJson.results[0].gender);
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>DOM Array Methods in React</h1>
+      <button onClick={addUserHandler}>Add User</button>
+      <div>{user}</div>
+      <input type="text" placeholder="search" />
+      <button id="sort-dsc">sort Desc</button>
+      <button id="sort-asc">sort Asc</button>
+      <div id="user-list"></div>
     </div>
   );
 }
