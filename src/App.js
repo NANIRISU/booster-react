304238704ddd0c43d5a9dd490e5095b307248a4e
NANIRISU <39872073+NANIRISU@users.noreply.github.com>
2021-04-27 23:57:17 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -3,45 +3,66 @@ import "./styles.css";
 
 export default function App() {
   const api = `https://randomuser.me/api`;
-
   const [user, setUser] = useState([]);
-
   const addUserHandler = async () => {
     const userData = await fetch(api, {
       method: "GET"
     });
     const userJson = await userData.json();
-    console.log(userJson.results[0]);
-    //user.push(userJson.results[0]);
+    //    console.log(userJson.results[0]);
     const newUser = [...user, userJson.results[0]];
-
     setUser(newUser);
   };
   return (
     <div className="main-container">
       <h1>DOM Array Methods in React</h1>
-      <div className="header-btns">
+      {/* <div className="header-btns">
         <button onClick={addUserHandler}>Add User</button>
         <input type="text" placeholder="search" />
         <button id="sort-dsc">sort Desc</button>
         <button id="sort-asc">sort Asc</button>
-      </div>
-      <div>
-        {user.map((userobj, index) => (
-          <div key={index} className="header-data">
-            <div className="data-name">
-              {" "}
-              {userobj.name.title} {userobj.name.first} {userobj.name.last}
-            </div>
-            <ol>
-              <li>{userobj.email} </li>
-              <li>{userobj.gender} </li>
-            </ol>
-          </div>
-        ))}
-      </div>
-
-      <div id="user-list"></div>
+      </div> */}
+      <UserBtn addUserHandler={addUserHandler} />
+      <UserList user={user} />
     </div>
   );
 }
+
+const UserList = (props) => {
+  const { user } = props;
+  return (
+    <div className="user-list">
+      {user.map((userObj, idx) => {
+        return <UserObject key={idx} userObj={userObj} />;
+      })}
+    </div>
+  );
+};
+
+const UserBtn = ({ addUserHandler }) => {
+  //const { addUserHandler = () => {} } = props;
+  return (
+    <div className="header-btns">
+      <button onClick={addUserHandler}>Add User</button>
+      <input type="text" placeholder="search" />
+      <button id="sort-dsc">sort Desc</button>
+      <button id="sort-asc">sort Asc</button>
+    </div>
+  );
+};
+
+const UserObject = (userobj) => {
+  return (
+    <div>
+      <div key={idx} className="header-data">
+        <div className="data-name">
+          {userobj.name.title} {userobj.name.first} {userobj.name.last}
+        </div>
+        <ol>
+          <li>{userobj.email} </li>
+          <li>{userobj.gender} </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
